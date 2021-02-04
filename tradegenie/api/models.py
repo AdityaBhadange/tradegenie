c8ca3923f9af0285e0fc25071626105ad53ae70f
Aditya Bhadange <aditya.bhadange18@gmail.com>
2021-02-04 19:10:11 +0530

    Added model fields as per requirement, excpet few

--- tradegenie/api/models.py
+++ tradegenie/api/models.py
@@ -27,174 +27,159 @@ class SellerProfile(models.Model):
 	ifsc = models.CharField(max_length=10)
 	branch_name = models.CharField(max_length=30)
 	google_pay_id = models.CharField(max_length=30, unique=True)
-	upi_id = models.CharField(max_length=30, unique=True)
+	upi_id = models.CharField(max_length=20, unique=True)
 	google_pay_number = models.CharField(max_length=20, unique=True)
 	country = models.ForeignKey(Country, on_delete=models.CASCADE)
 	group = models.ForeignKey(Group, on_delete=models.CASCADE)
 	date = models.DateTimeField(auto_now_add=True)
 
 
-# class Keyword(models.Model):
-# 	keyword_id = models.IntegerField(primary_key=True)
-# 	keyword_name = models.CharField(max_length=30)
+class Keyword(models.Model):
+	keyword_name = models.CharField(max_length=30)
 
-# 	class Meta:
-# 		db_table = "KeywordTable"
-
-
-# class KeywordUser(models.Model):
-# 	mapping_id = models.AutoField(primary_key=True)
-# 	user = models.ForeignKey(primary_key=True)
-# 	keyword_id = models.ForeignKey(primary_key=True)
-
-# 	class Meta:
-# 		db_table = "KeywordUser"
+	def __repr__(self):
+		return self.keyword_name
 
 
-# class Product(models.Model):
-# 	product_id = models.AutoField(primary_key=True)
-# 	user = models.ForeignKey()
-# 	catagory_id = models.ForeignKey()
-# 	description = models.CharField(max_length=100)
-# 	photos = models.FileField(upload_to="uploads/")
-# 	gst = models.CharField(max_length=30)
-# 	courier_self = models.CharField(max_length=30)
-# 	self_delivery_cost = models.IntegerField()
-# 	delivery_partner_id = models.ForeignKey("", on_delete=models.CASCADE)
-# 	length = models.IntegerField() # Query
-# 	width = models.IntegerField() # Query
-# 	height = models.AutoField() # Query
-# 	weight = models.IntegerField(max_length=30)
-# 	delivery_partner_cost = models.IntegerField()
-# 	product_cost = models.IntegerField()
-
-# 	class Meta:
-# 		db_table = "ProductTable"
+class KeywordUser(models.Model):
+	mapping_id = models.AutoField(primary_key=True)
+	user = models.ForeignKey(primary_key=True)
+	keyword_id = models.ForeignKey(primary_key=True)
 
 
-# class Catagory(models.Model):
-# 	catagory_id = models.AutoField(primary_key=True)
-# 	catagory_name = models.CharField(max_length=30)
-# 	photo = models.FileField(upload_to="uploads/")
+class Product(models.Model):
+	product_id = models.AutoField(primary_key=True)
+	user = models.ForeignKey(User, on_delete=CASCADE)
+	catagory_id = models.ForeignKey()
+	description = models.CharField(max_length=100)
+	photos = models.ImageField(upload_to="images/")
+	gst = models.CharField(max_length=30)
+	courier_self = models.CharField(max_length=30)
+	self_delivery_cost = models.DecimalField(max_digits=5, decimal_places=2)
+	delivery_partner = models.ForeignKey(DeliveryPartner, on_delete=models.CASCADE)
+	length = models.DecimalField(max_digits=5, decimal_places=2)
+	width = models.DecimalField(max_digits=5, decimal_places=2)
+	height = models.AutoField()
+	weight = models.DecimalField(max_length=30)
+	delivery_partner_cost = models.DecimalField(max_digits=5, decimal_places=2)
+	product_cost = models.DecimalField(max_digits=5, decimal_places=2)
 
 
-# class CategoryKeyword(models.Model):
-# 	table_primary_key = models.AutoField(primary_key=True)
-# 	keyword_id = models.ForeignKey()
-# 	catagory_id = models.ForeignKey()
-# 	commission = models.IntegerField()
+class Catagory(models.Model):
+	catagory_id = models.AutoField(primary_key=True)
+	catagory_name = models.CharField(max_length=30)
+	photo = models.ImageField(upload_to="images/")
 
-# 	class Meta:
-# 		db_table = "CategoryKeyword"
-
-
-# class DeliveryPartnerTable(models.Model):
-# 	delivery_partner_id = models.AutoField(primary_key=True)
-# 	name = models.CharField(max_length=30)
-
-# 	class Meta:
-# 		db_table = "DeliveryPartnerTable"
+	def __str__(self):
+		return self.catagory_name
 
 
-# class Order(models.Model):
-# 	order_id = models.AutoField(primary_key=True)
-# 	date_of_order = models.AutoField()
-# 	product_id = models.ForeignKey()
-# 	seller_id = models.ForeignKey()
-# 	buyer_id = models.ForeignKey()
-# 	quantity = models.IntegerField()
-# 	basic_cost = models.IntegerField()
-# 	tax = models.IntegerField()
-# 	delivery_cost = models.IntegerField()
-# 	total_cost = models.IntegerField()
-# 	status = models.CharField() # Query
-# 	tracking_id = models.IntegerField(unique=True)
-# 	delivery_partner_id = models.IntegerField()
-# 	shipment_date = models.DateTimeField()
-# 	group_id = models.ForeignKey()
-
-# 	class Meta:
-# 		db_table = "OrderTable"
+class CategoryKeyword(models.Model):
+	table_primary_key = models.AutoField(primary_key=True)
+	keyword = models.ForeignKey(Keyword, on_delete=CASCADE)
+	catagory = models.ForeignKey(Catagory, on_delete=CASCADE)
+	commission = models.DecimalField(max_digits=5, decimal_places=2)
 
 
-# class BuyerProfile(models.Model):
-# 	buyer_id = models.AutoField(primary_key=True)
-# 	user = models.ForeignKey()
-# 	delivery_name = models.CharField(max_length=30)
-# 	delivery_city = models.CharField(max_length=30)
-# 	delivery_address = models.CharField(max_length=50)
-# 	delivery_pin_code = models.IntegerField()
-# 	delivery_whatsapp_number = models.IntegerField()
-# 	billing_name = models.CharField(max_length=30)
-# 	billing_city = models.AutoField()
-# 	billing_address = models.AutoField()
-# 	billing_pin = models.AutoField()
-# 	billing_whatsapp_number = models.AutoField()
 
-# 	class Meta:
-# 		db_table = "BuyerProfile"
+class DeliveryPartner(models.Model):
+	delivery_partner_id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.name
 
 
-# class DeliveryPartnerOrder(models.Model):
-# 	table_key = models.AutoField(primary_key=True)
-# 	order_id = models.ForeignKey()
-# 	user = models.ForeignKey()
-# 	delivery_status = models.CharField(max_length=30)
-# 	tracking_id = models.IntegerField(unique=True)
-# 	shipment_date = models.DateTimeField()
-
-# 	class Meta:
-# 		db_table = "DeliveryPartnerOrderTable"
-
-
-# class Country(models.Model):
-# 	country_key = models.AutoField(primary_key=True)
-# 	country_name = models.CharField(max_length=30)
-
-# 	class Meta:
-# 		db_table = "CountryTable"
+class Order(models.Model):
+	order_id = models.AutoField(primary_key=True)
+	date_of_order = models.DateTimeField(auto_now_add=True)
+	product = models.ForeignKey(product, on_delete=CASCADE)
+	seller = models.ForeignKey(SellerProfile, on_delete=CASCADE)
+	buyer = models.ForeignKey(BuyerProfile, on_delete=CASCADE)
+	quantity = models.DecimalField(max_digits=5, decimal_places=2)
+	basic_cost = models.DecimalField(max_digits=5, decimal_places=2)
+	tax = models.DecimalField(max_digits=5, decimal_places=2)
+	delivery_cost = models.DecimalField(max_digits=5, decimal_places=2)
+	total_cost = models.DecimalField(max_digits=5, decimal_places=2)
+	status = models.CharField(max_length=10)
+	# tracking_id = models.IntegerField(unique=True) # How to create pk for this?
+	delivery_partner = models.ForeignKey(DeliveryPartner, on_delete=CASCADE)
+	shipment_date = models.DateTimeField(auto_now_add=True)
+	group = models.ForeignKey(Group, on_delete=CASCADE)
 
 
-# class City(models.Model):
-# 	city_key = models.AutoField(primary_key=True)
-# 	city_name = models.AutoField(primary_key=True)
-# 	country_key = models.ForeignKey()
+class BuyerProfile(models.Model):
+	buyer_id = models.AutoField(primary_key=True)
+	user = models.ForeignKey(User, on_delete=CASCADE)
+	delivery_name = models.CharField(max_length=30)
+	delivery_city = models.CharField(max_length=30)
+	delivery_address = models.CharField(max_length=50)
+	delivery_pin_code = models.DecimalField(max_digits=5, decimal_places=2)
+	delivery_whatsapp_number = models.CharField(max_length=20)
+	billing_name = models.CharField(max_length=30)
+	billing_city = models.CharField(max_length=30)
+	billing_address = models.CharField(max_length=50)
+	billing_pin_code = models.DecimalField(max_digits=5, decimal_places=2)
+	billing_whatsapp_number = models.CharField(max_length=20)
 
 
-# class Tax(models.Model):
-# 	tax_id = models.AutoField(primary_key=True)
-# 	tax_name = models.CharField(max_length=30)
-# 	tax_percentage = models.CharField(max_length=30)
-# 	tax_name = models.CharField(max_length=30)
+class DeliveryPartnerOrder(models.Model):
+	table_key = models.AutoField(primary_key=True)
+	order = models.ForeignKey(Order, on_delete=CASCADE)
+	user = models.ForeignKey(User, on_delete=CASCADE)
+	delivery_status = models.CharField(max_length=10)
+	# tracking_id = models.DecimalField(unique=True) # How to create an ID for this field?
+	shipment_date = models.DateTimeField(auto_now_add=True)
 
 
-# class Notification(models.Model):
-# 	notification_id = models.AutoField(primary_key=True)
-# 	user = models.ForeignKey()
-# 	notification_message = models.CharField(max_length=50)
+class Country(models.Model):
+	country_key = models.AutoField(primary_key=True)
+	country_name = models.CharField(max_length=20)
+
+	def __repr__(self):
+		return self.country_name
 
 
-# class Group(models.Model):
-# 	group_id = models.AutoField(primary_key=True)
-# 	group_description = models.CharField(max_length=50)
-# 	date_of_addition = models.DateTimeField()
+class City(models.Model):
+	city_key = models.AutoField(primary_key=True)
+	city_name = models.CharField(max_length=20)
+	country_key = models.ForeignKey(Country, on_delete=CASCADE)
 
 
-# class CommissionGroup(models.Model):
-# 	table_key = models.AutoField(primary_key=True)
-# 	group_id = models.ForeignKey()
-# 	catagory_id = models.IntegerField(unique=True)
-# 	company_commission = models.IntegerField()
-# 	group_commission = models.IntegerField()
-# 	user = models.ForeignKey()
+class Tax(models.Model):
+	tax_id = models.AutoField(primary_key=True)
+	tax_name = models.CharField(max_length=30)
+	tax_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+	tax_name = models.CharField(max_length=20)
 
 
-# class Auth(models.Model):
-# 	table_key = models.AutoField(primary_key=True)
-# 	user = models.ForeignKey()
-# 	super_admin = models.CharField()
-# 	group_admin = models.CharField()
-# 	delivery_admin = models.CharField()
-# 	group_id = models.ForeignKey()
-# 	screen_id_1 = models.IntegerField(unique=True)
-# 	screen_id_2 = models.IntegerField(unique=True)
+class Notification(models.Model):
+	notification_id = models.AutoField(primary_key=True)
+	user = models.ForeignKey(User, on_delete=CASCADE)
+	notification_message = models.CharField(max_length=50)
+
+
+class Group(models.Model):
+	group_id = models.AutoField(primary_key=True)
+	group_description = models.CharField(max_length=50)
+	date_of_addition = models.DateTimeField(auto_now_add=True)
+
+
+class CommissionGroup(models.Model):
+	table_key = models.AutoField(primary_key=True)
+	group = models.ForeignKey(Group, on_delete=CASCADE)
+	catagory = models.ForeignKey(Catagory, on_delete=CASCADE)
+	company_commission = models.DecimalField(max_digits=5, decimal_places=2)
+	group_commission = models.DecimalField(max_digits=5, decimal_places=2)
+	user = models.ForeignKey(User, on_delete=CASCADE)
+
+
+class Auth(models.Model):
+	table_key = models.AutoField(primary_key=True)
+	user = models.ForeignKey(User, on_delete=CASCADE)
+	super_admin = models.CharField(max_length=10)
+	group_admin = models.CharField(max_length=10)
+	delivery_admin = models.CharField(max_length=10)
+	group = models.ForeignKey(Group, on_delete=CASCADE)
+	screen_id_1 = models.BooleanField(default=False)
+	screen_id_2 = models.BooleanField(default=False)
