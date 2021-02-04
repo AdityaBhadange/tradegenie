e8380e693fbf73c82fb3ffb4c126ee2117ae9c99
Aditya Bhadange <aditya.bhadange18@gmail.com>
2021-02-04 20:11:30 +0530

    Added migrations

--- tradegenie/api/models.py
+++ tradegenie/api/models.py
@@ -7,7 +7,7 @@ Author - Aditya Bhadange
 Few Notes:-
 	
 	1. Removed all the pk columns, bcs Django adds it for all the tables automatically.
-	2. For the ForiegnKey fields, I have changed the variable names (e.g. 'user_id' to 'user').
+	2. For the ForeignKey fields, I have changed the variable names (e.g. 'user_id' to 'user').
 	3. Added '__repr__' function wherever necessery for readability.
 	4. For phone numbers fields, I have used CharField.
 	5. For images, I have used ImageField instead of FileField, because it checks for image formats.
@@ -22,7 +22,7 @@ class User(models.Model):
 	city = models.CharField(max_length=30)
 	seller = models.CharField(max_length=1)
 	whatsapp_number = models.CharField(max_length=20, unique=True)
-	group = models.ForeignKey(Group, on_delete=CASCADE)
+	group = models.ForeignKey('Group', on_delete=models.CASCADE)
 	date = models.DateTimeField(auto_now_add=True)
 
 	def __repr__(self):
@@ -30,7 +30,7 @@ class User(models.Model):
 
 
 class SellerProfile(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	user = models.ForeignKey('User', on_delete=models.CASCADE)
 	city = models.CharField(max_length=30)
 	address = models.CharField(max_length=50)
 	pin_code = models.CharField(max_length=10)
@@ -42,8 +42,8 @@ class SellerProfile(models.Model):
 	google_pay_id = models.CharField(max_length=30, unique=True)
 	upi_id = models.CharField(max_length=20, unique=True)
 	google_pay_number = models.CharField(max_length=20, unique=True)
-	country = models.ForeignKey(Country, on_delete=models.CASCADE)
-	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+	country = models.ForeignKey('Country', on_delete=models.CASCADE)
+	group = models.ForeignKey('Group', on_delete=models.CASCADE)
 	date = models.DateTimeField(auto_now_add=True)
 
 
@@ -55,23 +55,23 @@ class Keyword(models.Model):
 
 
 class KeywordUser(models.Model):
-	user = models.ForeignKey(User, on_delete=CASCADE)
-	keyword = models.ForeignKey(Keyword, on_delete=CASCADE)
+	user = models.ForeignKey('User', on_delete=models.CASCADE)
+	keyword = models.ForeignKey('Keyword', on_delete=models.CASCADE)
 
 
 class Product(models.Model):
-	user = models.ForeignKey(User, on_delete=CASCADE)
-	catagory_id = models.ForeignKey(Catagory, on_delete=CASCADE)
+	user = models.ForeignKey('User', on_delete=models.CASCADE)
+	catagory_id = models.ForeignKey('Catagory', on_delete=models.CASCADE)
 	description = models.CharField(max_length=100)
 	photos = models.ImageField(upload_to="images/")
 	gst = models.CharField(max_length=30)
 	courier_self = models.CharField(max_length=30)
 	self_delivery_cost = models.DecimalField(max_digits=5, decimal_places=2)
-	delivery_partner = models.ForeignKey(DeliveryPartner, on_delete=models.CASCADE)
+	delivery_partner = models.ForeignKey('DeliveryPartner', on_delete=models.CASCADE)
 	length = models.DecimalField(max_digits=5, decimal_places=2)
 	width = models.DecimalField(max_digits=5, decimal_places=2)
-	height = models.AutoField()
-	weight = models.DecimalField(max_length=30)
+	height = models.DecimalField(max_digits=5, decimal_places=2)
+	weight = models.DecimalField(max_digits=5, decimal_places=2)
 	delivery_partner_cost = models.DecimalField(max_digits=5, decimal_places=2)
 	product_cost = models.DecimalField(max_digits=5, decimal_places=2)
 
@@ -85,8 +85,8 @@ class Catagory(models.Model):
 
 
 class CategoryKeyword(models.Model):
-	keyword = models.ForeignKey(Keyword, on_delete=CASCADE)
-	catagory = models.ForeignKey(Catagory, on_delete=CASCADE)
+	keyword = models.ForeignKey('Keyword', on_delete=models.CASCADE)
+	catagory = models.ForeignKey('Catagory', on_delete=models.CASCADE)
 	commission = models.DecimalField(max_digits=5, decimal_places=2)
 
 
@@ -100,9 +100,9 @@ class DeliveryPartner(models.Model):
 
 class Order(models.Model):
 	date_of_order = models.DateTimeField(auto_now_add=True)
-	product = models.ForeignKey(product, on_delete=CASCADE)
-	seller = models.ForeignKey(SellerProfile, on_delete=CASCADE)
-	buyer = models.ForeignKey(BuyerProfile, on_delete=CASCADE)
+	product = models.ForeignKey('product', on_delete=models.CASCADE)
+	seller = models.ForeignKey('SellerProfile', on_delete=models.CASCADE)
+	buyer = models.ForeignKey('BuyerProfile', on_delete=models.CASCADE)
 	quantity = models.DecimalField(max_digits=5, decimal_places=2)
 	basic_cost = models.DecimalField(max_digits=5, decimal_places=2)
 	tax = models.DecimalField(max_digits=5, decimal_places=2)
@@ -110,13 +110,13 @@ class Order(models.Model):
 	total_cost = models.DecimalField(max_digits=5, decimal_places=2)
 	status = models.CharField(max_length=10)
 	# tracking_id = models.IntegerField(unique=True) # How to create pk for this?
-	delivery_partner = models.ForeignKey(DeliveryPartner, on_delete=CASCADE)
+	delivery_partner = models.ForeignKey('DeliveryPartner', on_delete=models.CASCADE)
 	shipment_date = models.DateTimeField(auto_now_add=True)
-	group = models.ForeignKey(Group, on_delete=CASCADE)
+	group = models.ForeignKey('Group', on_delete=models.CASCADE)
 
 
 class BuyerProfile(models.Model):
-	user = models.ForeignKey(User, on_delete=CASCADE)
+	user = models.ForeignKey('User', on_delete=models.CASCADE)
 	delivery_name = models.CharField(max_length=30)
 	delivery_city = models.CharField(max_length=30)
 	delivery_address = models.CharField(max_length=50)
@@ -130,8 +130,8 @@ class BuyerProfile(models.Model):
 
 
 class DeliveryPartnerOrder(models.Model):
-	order = models.ForeignKey(Order, on_delete=CASCADE)
-	user = models.ForeignKey(User, on_delete=CASCADE)
+	order = models.ForeignKey('Order', on_delete=models.CASCADE)
+	user = models.ForeignKey('User', on_delete=models.CASCADE)
 	delivery_status = models.CharField(max_length=10)
 	# tracking_id = models.DecimalField(unique=True) # How to create an ID for this field?
 	shipment_date = models.DateTimeField(auto_now_add=True)
@@ -146,17 +146,17 @@ class Country(models.Model):
 
 class City(models.Model):
 	city_name = models.CharField(max_length=20)
-	country_key = models.ForeignKey(Country, on_delete=CASCADE)
+	country_key = models.ForeignKey('Country', on_delete=models.CASCADE)
 
 
 class Tax(models.Model):
 	tax_name = models.CharField(max_length=30)
 	tax_percentage = models.DecimalField(max_digits=5, decimal_places=2)
-	country = models.ForeignKey(Country, on_delete=CASCADE)
+	country = models.ForeignKey('Country', on_delete=models.CASCADE)
 
 
 class Notification(models.Model):
-	user = models.ForeignKey(User, on_delete=CASCADE)
+	user = models.ForeignKey('User', on_delete=models.CASCADE)
 	notification_message = models.CharField(max_length=50)
 
 
@@ -166,18 +166,18 @@ class Group(models.Model):
 
 
 class CommissionGroup(models.Model):
-	group = models.ForeignKey(Group, on_delete=CASCADE)
-	catagory = models.ForeignKey(Catagory, on_delete=CASCADE)
+	group = models.ForeignKey('Group', on_delete=models.CASCADE)
+	catagory = models.ForeignKey('Catagory', on_delete=models.CASCADE)
 	company_commission = models.DecimalField(max_digits=5, decimal_places=2)
 	group_commission = models.DecimalField(max_digits=5, decimal_places=2)
-	user = models.ForeignKey(User, on_delete=CASCADE)
+	user = models.ForeignKey('User', on_delete=models.CASCADE)
 
 
 class Auth(models.Model):
-	user = models.ForeignKey(User, on_delete=CASCADE)
+	user = models.ForeignKey('User', on_delete=models.CASCADE)
 	super_admin = models.CharField(max_length=10)
 	group_admin = models.CharField(max_length=10)
 	delivery_admin = models.CharField(max_length=10)
-	group = models.ForeignKey(Group, on_delete=CASCADE)
+	group = models.ForeignKey('Group', on_delete=models.CASCADE)
 	screen_id_1 = models.BooleanField(default=False)
 	screen_id_2 = models.BooleanField(default=False)
