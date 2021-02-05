ea66d0170d765b441c55ed64c05697f1d85b0451
Aditya Bhadange <aditya.bhadange18@gmail.com>
2021-02-05 15:41:51 +0530

    Implemented generic class-based views

--- tradegenie/api/views.py
+++ tradegenie/api/views.py
@@ -6,61 +6,21 @@ from rest_framework import mixins
 from rest_framework import generics
 
 
-class UserList(mixins.ListModelMixin,
-			mixins.CreateModelMixin,
-			generics.GenericAPIView):
+class UserList(generics.ListCreateAPIViews):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
 
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
-
-
-class UserDetail(mixins.RetrieveModelMixin,
-				mixins.UpdateModelMixin,
-				mixins.DestroyModelMixin,
-				generics.GenericAPIView):
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.retrieve(request, *args, **kwargs)
 
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
-
-	def delete(self, request, *args, **kwargs):
-		return self.destroy(request, *args, **kwargs)
-
-
-class GroupList(mixins.ListModelMixin,
-				mixins.CreateModelMixin,
-				generics.GenericAPIView):
+class GroupList(generics.ListCreateAPIViews):
 	queryset = Group.objects.all()
 	serializer_class = GroupSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
 
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
-
-
-class GroupDetail(mixins.RetrieveModelMixin,
-				mixins.UpdateModelMixin,
-				mixins.DestroyModelMixin,
-				generics.GenericAPIView):
+class GroupDetail(generics.RetrieveUpdateDestroyAPIViewd):
 	queryset = Group.objects.all()
 	serializer_class = GroupSerializer
-
-	def get(self, request, *args, **kwargs):
-		return self.retrieve(request, *args, **kwargs)
-
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
-
-	def delete(self, request, *args, **kwargs):
-		return self.destroy(request, *args, **kwargs)
